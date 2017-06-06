e743baa6b819ccc7f111c543cdd167d75fc1ede8
MFoster <mattfoster01@gmail.com>
2017-06-06 00:22:53 -0400

    more towards coverage

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,23 @@ module.exports = {
     entry: "./src/broadcaster.js",
     module: {
 		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "babel-loader"
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "eslint-loader"
-			}
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "eslint-loader"
+                },
+                // instrument only testing sources with Istanbul
+                {
+                    test: /\.js$/,
+                    include: path.resolve("src/"),
+                    loader: "istanbul-instrumenter-loader"
+                }
+            ]
 		]
 	},
 	resolve: {
